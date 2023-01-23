a2c2f210068edc071607cc84ceb70543b5c1c163
batuklambu <batuklambu@gmail.com>
2023-01-23 09:53:48 -0500

    ModifyPics

--- src/components/EmployeeList.js
+++ src/components/EmployeeList.js
@@ -4,10 +4,14 @@ import Image from './Image';
 function EmployeeList(props) {
   return (
     <div className="employeelistitem">
-      <Image className="img" img={props.img} alt="img" />
-      {props.name}
-      {props.lName}
-      <p className="degination">{props.designation}</p>
+      <div>
+        <Image className="img" img={props.img} alt="img" />
+      </div>
+      <div className="employee-info">
+        {props.name}
+        {props.lName}
+        <p className="degination">{props.designation}</p>
+      </div>
     </div>
   );
 }
